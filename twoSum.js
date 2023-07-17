39291dc19b0fd3c8d54e82b9833929770329f409
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-07-17 19:38:35 +0300

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -12,18 +12,17 @@ Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
 
-let twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
+let twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++){
+    for(let j = i + 1; j < nums.length; j++){
+      if (nums[i] + nums[j] == target){
+        return [i + j], target
       }
     }
   }
-};
+}
 
-console.log(twoSum([1, 2, 3, 4], 6));
-
+console.log(twoSum([1,2,3,4,5], 5));
 
 // understood 
 
